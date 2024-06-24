80c8df027c250458736783f8dcef7eb582ed7968
Ramanujam Gond <ramanujamgond@gmail.com>
2024-06-24 16:52:11 +0530

    fix: resolve the issue of generateStaticParams for generating dynamic routes

--- src/app/hotel-details/[...slug]/page.tsx
+++ src/app/hotel-details/[...slug]/page.tsx
@@ -1,124 +1,28 @@
-"use client";
-import HotelAboutUsSection from "@/components/hoteldetails/hotelaboutsection/HotelAboutUsSection";
-import HotelDetailSlider from "@/components/hoteldetails/hoteldetailslider/HotelDetailSlider";
-import HotelMapPolicyDetails from "@/components/hoteldetails/hotelmappolicydetails/HotelMapPolicyDetails";
-import HotelRoomType from "@/components/hoteldetails/hotelroomtype/HotelRoomType";
-import RoomCart from "@/components/hoteldetails/roomcart/RoomCart";
-import Loader from "@/components/ui/loader";
-import useHotelDetails from "@/hooks/useHotelDetails";
-import useHotelRoomTypes from "@/hooks/useHotelRoomTypes";
+import HotelDetailsClient from "./HotelDetailsClient";
 import { formatString } from "@/lib/utils";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 interface Params {
-  slug: string;
+  params: {
+    slug: string[];
+  };
 }
 
-interface HotelDetailsProps {
-  params: Params;
+export async function generateStaticParams() {
+  // Normally you would return a list of known parameters here.
+  // Return an empty array if you don't have a predefined list.
+  return [];
 }
 
-const HotelDetailsPage: React.FC<HotelDetailsProps> = ({ params }) => {
-  // next 14 router
-  const router = useRouter();
-
-  // get the url params
-  const urlParms = params;
-
+const HotelDetailsPage = ({ params }: Params) => {
   // extract data from the slug object of url params
-  const hotelId = urlParms.slug[0];
-  const hotelName = urlParms.slug[1];
+  const hotelId = params.slug[0];
+  const hotelName = params.slug[1];
 
   // call a utility method to fromat the string in proper readable format
-  const formatedHotelName = formatString(hotelName);
-
-  // go back[go back in the browser stack]
-  const navigateBack = () => {
-    router.back();
-  };
-
-  const { loading, hotelDetails, fetchHotelDetails } = useHotelDetails({
-    hotelId,
-  });
-
-  const { invLoading, roomTypeInventory, fetchRoomTypesInventory } =
-    useHotelRoomTypes({
-      hotelId,
-    });
-
-  console.log("roomTypeInventory", roomTypeInventory);
-
-  useEffect(() => {
-    fetchHotelDetails();
-  }, []);
-
-  useEffect(() => {
-    fetchRoomTypesInventory();
-  }, []);
-
-  // standard loader
-  if (loading && invLoading) {
-    return <Loader />;
-  }
+  const formattedHotelName = formatString(hotelName);
 
   return (
-    <div className="my-12 h-full min-h-[90vh]">
-      <div className="pt-2">
-        <div className="container">
-          <div className="flex items-center justify-between mt-14">
-            <div className="text-base font-normal text-[#656565]">
-              <span
-                className="hover:text-[#FF6535] cursor-pointer"
-                onClick={navigateBack}
-              >
-                Book Now
-              </span>{" "}
-              {"> "}
-              <span className="font-bold text-[#141414]">
-                {formatedHotelName}
-              </span>
-            </div>
-            <div></div>
-          </div>
-          <div className="mt-5 mb-5">
-            {hotelDetails && (
-              <HotelDetailSlider
-                exterior_images={hotelDetails?.exterior_images}
-              />
-            )}
-            {hotelDetails && (
-              <HotelAboutUsSection
-                hotel_name={hotelDetails.hotel_name}
-                city_name={hotelDetails.city_name}
-                address={hotelDetails.address}
-                hotel_description={hotelDetails.hotel_description}
-                facility={hotelDetails.facility}
-                star={hotelDetails.star}
-              />
-            )}
-
-            {roomTypeInventory.length > 0 && (
-              <HotelRoomType roomTypeInventory={roomTypeInventory} />
-            )}
-
-            {hotelDetails && (
-              <HotelMapPolicyDetails
-                hotel_policy={hotelDetails?.hotel_policy}
-                child_policy={hotelDetails?.child_policy}
-                cancellation_policy={hotelDetails?.cancel_policy}
-                latitude={hotelDetails?.latitude}
-                longitude={hotelDetails?.longitude}
-                city_name={hotelDetails.city_name}
-                address={hotelDetails.address}
-              />
-            )}
-
-            <RoomCart />
-          </div>
-        </div>
-      </div>
-    </div>
+    <HotelDetailsClient hotelId={hotelId} hotelName={formattedHotelName} />
   );
 };
 
